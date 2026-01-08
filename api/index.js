92d4f2058d3c13921c721a4dc7b071e667d9c52c
Can Aydın <canaydinn@gmail.com>
2026-01-08 14:12:27 +0300

    vercel. json devam

--- api/index.js
+++ api/index.js
@@ -5,6 +5,5 @@ const app = require('./src/app');
 
 // Vercel Serverless Function için export
 // Vercel Express app'i otomatik olarak handle eder
-// Hem app hem de handler formatını destekler
 module.exports = app;
 
