9a1a863bf0a1a5a62d40e162f18d64b5a1077968
Can Aydın <canaydinn@gmail.com>
2026-01-09 11:28:39 +0300

    users güncellendi

--- admin/assets/js/users.js
+++ admin/assets/js/users.js
@@ -1,4 +1,7 @@
-const API_BASE_URL = 'http:// /api';
+const API_BASE_URL =
+  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:4000/api'   // Lokal
+    : window.location.origin + '/api'; 
 
 const ROLE_LABELS = {
   1: 'Belediye Yöneticisi',
